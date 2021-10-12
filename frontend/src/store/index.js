0df13160ecd3295b1abff548a631ae717e961d82
byunghun-jake <figma@kakao.com>
2021-10-12 21:08:37 +0900

    feat: api, store 모듈화, vue 포트변경 8080 => 8081

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,8 +1,11 @@
 import { createStore } from "vuex"
 
+import auth from "@/store/modules/auth"
+import project from "@/store/modules/project"
+
 export default createStore({
   state: {},
   mutations: {},
   actions: {},
-  modules: {},
+  modules: { auth, project },
 })
