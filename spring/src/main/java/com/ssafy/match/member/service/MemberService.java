5aa84d557d762cd0807fe91e7ab5c0550b2614e3
ilminmoon <ian.ilminmoon@gmail.com>
2021-10-13 16:21:53 +0900

    position update

--- spring/src/main/java/com/ssafy/match/member/service/MemberService.java
+++ spring/src/main/java/com/ssafy/match/member/service/MemberService.java
@@ -147,14 +147,14 @@ public class MemberService {
         return HttpStatus.OK;
     }
 
-//    @Transactional
-//    public HttpStatus updateMemberSkill(MemberSkillRequestDto memberSkillRequestDto) {
-//        Member member = memberRepository.findById(SecurityUtil.getCurrentMemberId()).orElseThrow(() -> new NullPointerException("토큰이 잘못되었거나 존재하지 않는 사용자입니다."));
-//        validPosition();
-//        validDPosition();
-//        validTechstack();
-//
-//    }
+    @Transactional
+    public HttpStatus updateMemberSkill(MemberSkillRequestDto memberSkillRequestDto) {
+        Member member = memberRepository.findById(SecurityUtil.getCurrentMemberId()).orElseThrow(() -> new NullPointerException("토큰이 잘못되었거나 존재하지 않는 사용자입니다."));
+        validPosition();
+        validDPosition();
+        validTechstack();
+
+    }
 
     @Transactional
     public MemberUpdateResponseDto updateMyInfo(MemberUpdateRequestDto memberUpdateRequestDto) throws Exception {
