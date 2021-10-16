63e575e257ce975914543a3c24a562dc54ed72fa
최영수 <soc4585@gmail.com>
2021-10-16 18:29:01 +0900

    Feat: 머지 충돌난 부분 수정

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -5,11 +5,10 @@ import Home from "@/views/Home.vue"
 import AuthLayout from "@/layouts/Auth.vue"
 import ProfileLayout from "@/layouts/Profile.vue"
 import ProjectLayout from "@/layouts/Project.vue"
-import Login from "@/views/Login.vue"
-import ProjectList from "@/views/ProjectList.vue"
+
 import Profile from "@/views/Profile.vue"
-import ProjectForm from "@/views/ProjectForm.vue"
 import ProjectList from "@/views/ProjectList.vue"
+import ProjectForm from "@/views/ProjectForm.vue"
 import Project from "@/views/Project.vue"
 
 import Login from "@/views/auth/Login.vue"
