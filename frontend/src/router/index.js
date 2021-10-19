7de9709e36f15ace1b3beb628810327f24c2a875
byunghun-jake <figma@kakao.com>
2021-10-19 22:14:01 +0900

    update: 회원가입 흐름 변경으로 인한 페이지 제거거

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -13,8 +13,6 @@ import Project from "@/views/Project.vue"
 
 import Login from "@/views/auth/Login.vue"
 import Signup from "@/views/auth/Signup.vue"
-import CheckEmail from "@/views/auth/CheckEmail.vue"
-import AuthEmail from "@/views/auth/AuthEmail.vue"
 import FindPassword from "@/views/auth/FindPassword.vue"
 
 const routes = [
@@ -26,20 +24,6 @@ const routes = [
   {
     path: "/auth",
     component: AuthLayout,
-    // beforeEnter: (to, from, next) => {
-    //   if (to.name === "Login" || to.name === "FindPassword") {
-    //     next()
-    //     return
-    //   }
-    //   if (!from.name) {
-    //     const signupStep = store.getters["auth/getSignupStep"]
-    //     if (to.name !== signupStep) {
-    //       next({ name: signupStep })
-    //       return
-    //     }
-    //   }
-    //   next()
-    // },
     meta: { requiresNoAuth: true },
     children: [
       {
@@ -51,16 +35,6 @@ const routes = [
         path: "signup",
         name: "Signup",
         component: Signup,
-      },
-      {
-        path: "check-email",
-        name: "CheckEmail",
-        component: CheckEmail,
-      },
-      {
-        path: "auth-email",
-        name: "AuthEmail",
-        component: AuthEmail,
       },
       {
         path: "find-password",
