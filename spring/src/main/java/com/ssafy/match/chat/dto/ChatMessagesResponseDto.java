6d7c9117297698cc5c3e88c788f48b49405c7391
ilminmoon <ian.ilminmoon@gmail.com>
2021-10-20 18:31:47 +0900

    chat history refactoring fin

--- spring/src/main/java/com/ssafy/match/chat/dto/ChatMessagesResponseDto.java
+++ spring/src/main/java/com/ssafy/match/chat/dto/ChatMessagesResponseDto.java
@@ -1,19 +1,24 @@
 package com.ssafy.match.chat.dto;
 
-import com.ssafy.match.chat.entity.ChatMessage;
 import com.ssafy.match.common.dto.DetailPositionInterface;
-import com.ssafy.match.file.dto.DBFileDto;
 import lombok.Builder;
 import lombok.Data;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
 public class ChatMessagesResponseDto {
-    private List<DetailPositionInterface> dpositionList = new ArrayList<>();
+    private List<ChatMessageInterface> chatMessageInterfaces = new ArrayList<>();
 
+    public static ChatMessagesResponseDto of(List<ChatMessageInterface> chatMessageInterfaces) {
+        return ChatMessagesResponseDto.builder()
+                .chatMessageInterfaces(chatMessageInterfaces)
+                .build();
+    }
 
-
+    @Builder
+    public ChatMessagesResponseDto(List<ChatMessageInterface> chatMessageInterfaces) {
+        this.chatMessageInterfaces = chatMessageInterfaces;
+    }
 }
