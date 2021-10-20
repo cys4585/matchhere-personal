bdbf88a08c4671e2bb89351203af788c040b14c1
ilminmoon <ian.ilminmoon@gmail.com>
2021-10-20 18:27:30 +0900

    chatMessageResponseDto createD

---
+++ spring/src/main/java/com/ssafy/match/chat/dto/ChatMessageResponseDto.java
@@ -0,0 +1,34 @@
+package com.ssafy.match.chat.dto;
+
+
+import com.ssafy.match.chat.entity.ChatMessage;
+import com.ssafy.match.file.dto.DBFileDto;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class ChatMessageResponseDto {
+    private String message;
+    private String nickname;
+    private LocalDateTime sentTime;
+    private DBFileDto dbFile;
+
+    public static ChatMessageResponseDto of(ChatMessage chatMessage) {
+        return ChatMessageResponseDto.builder()
+                .message(chatMessage.getMessage())
+                .nickname(chatMessage.getNickname())
+                .sentTime(chatMessage.getSent_time())
+                .dbFile(DBFileDto.of(chatMessage.getDbFile()))
+                .build();
+    }
+
+    @Builder
+    public ChatMessageResponseDto(String message, String nickname, LocalDateTime sentTime, DBFileDto dbFile) {
+        this.message = message;
+        this.nickname = nickname;
+        this.sentTime = sentTime;
+        this.dbFile = dbFile;
+    }
+}
--- spring/src/main/java/com/ssafy/match/chat/dto/ChatMessagesResponseDto.java
+++ spring/src/main/java/com/ssafy/match/chat/dto/ChatMessagesResponseDto.java
@@ -13,25 +13,7 @@ import java.util.List;
 @Data
 public class ChatMessagesResponseDto {
     private List<DetailPositionInterface> dpositionList = new ArrayList<>();
-//    private String message;
-//    private String nickname;
-//    private LocalDateTime sentTime;
-//    private DBFileDto dbFile;
 
-    public static ChatMessagesResponseDto of(ChatMessage chatMessage) {
-        return ChatMessagesResponseDto.builder()
-                .message(chatMessage.getMessage())
-                .nickname(chatMessage.getNickname())
-                .sentTime(chatMessage.getSent_time())
-                .dbFile(DBFileDto.of(chatMessage.getDbFile()))
-                .build();
-    }
 
-    @Builder
-    public ChatMessagesResponseDto(String message, String nickname, LocalDateTime sentTime, DBFileDto dbFile) {
-        this.message = message;
-        this.nickname = nickname;
-        this.sentTime = sentTime;
-        this.dbFile = dbFile;
-    }
+
 }
