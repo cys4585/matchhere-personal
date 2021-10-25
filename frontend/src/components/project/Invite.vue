96cc61dbb47169e97dbcaabf092e9ccaa5fe0426
최영수 <soc4585@gmail.com>
2021-10-25 21:51:02 +0900

    Feat: 주소 복사

--- frontend/src/components/project/Invite.vue
+++ frontend/src/components/project/Invite.vue
@@ -6,15 +6,31 @@
     >
       링크를 공유하고, 팀원을 초대하세요!
     </div>
-    <button class="rounded-md bg-blue-500 text-white py-2">
+    <button class="rounded-md bg-blue-500 text-white py-2" @click="handleClick">
       초대 링크 복사하기
     </button>
+    <input type="hidden" :value="path" id="path" />
   </div>
 </template>
 
 <script>
+import { ref } from "@vue/reactivity"
+import { useRoute } from "vue-router"
 export default {
   name: "Invite",
+  setup() {
+    const route = useRoute()
+
+    const path = ref(`matchere.me/projects/article/${route.params.projectId}`)
+    const handleClick = () => {
+      const path = document.getElementById("path")
+      path.setAttribute("type", "text")
+      path.select()
+      document.execCommand("copy")
+      path.setAttribute("type", "hidden")
+    }
+    return { path, handleClick }
+  },
 }
 </script>
 
